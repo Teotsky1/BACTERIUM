f1b56e547a7ace81c931277ccca4f4188a08661a
Teotsky1 <mateo.arangog@upb.edu.co>
2023-03-27 01:04:37 -0500

    Basic PlayerMov

--- Assets/PROGRAMACION/PlayerMovement.cs
+++ Assets/PROGRAMACION/PlayerMovement.cs
@@ -1,15 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
 
-    private float speed = 5f;
-
-    private Rigidbody2D playerRB;
-
     
+    //Fisicas
+    private Rigidbody2D playerRB;
+    [SerializeField] private float speed = 5f;
+
+    //Vector 2D
+    private Vector2 moveInput;
+
+
+    /*
+    //DASH
+    [SerializeField] private float dashCoolDown;
+    [SerializeField] private float dashSpeed = 15f;
+
+    */
+
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,7 +33,19 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Inputs
         float moveX = Input.GetAxis("Horizontal");
+        float moveY = Input.GetAxis("Vertical");
+        moveInput = new Vector2(moveX, moveY).normalized;
+        
+
+    }
+
+
+    private void FixedUpdate()
+    {
+        //Fisicas
+         playerRB.MovePosition(playerRB.position + moveInput * speed * Time.fixedDeltaTime);
 
     }
 }
