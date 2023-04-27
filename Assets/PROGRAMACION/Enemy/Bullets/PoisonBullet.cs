60845265cd2c3afdc1274c289b378255c37eab7b
Teotsky1 <mateo.arangog@upb.edu.co>
2023-04-26 22:55:24 -0500

    Poison Bullet

--- Assets/PROGRAMACION/Enemy/Bullets/PoisonBullet.cs
+++ Assets/PROGRAMACION/Enemy/Bullets/PoisonBullet.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class PoisonBullet : MonoBehaviour
 {
-    // Start is called before the first frame update
+    GameObject target;
+    public float speed;
+    Rigidbody2D bulletRB;
+
+    [Header("DAÑO")]
+    [SerializeField] float daño;
+
+
+    public bool TocaJugador;
+
     void Start()
     {
-        
+        bulletRB = GetComponent<Rigidbody2D>();
+        target = GameObject.FindGameObjectWithTag("Player");
+        Vector2 moveDir = (target.transform.position - transform.position).normalized * speed;
+        bulletRB.velocity = new Vector2(moveDir.x, moveDir.y);
+        Destroy(this.gameObject, 1.5f);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if (collision.gameObject.GetComponent<SistemaDeVida>() != null)
+        {
+
+            collision.gameObject.GetComponent<SistemaDeVida>().QuitarVida(daño);
+            if (collision.CompareTag("Player"))
+                Destroy(this.gameObject);
+        }
+
+
     }
+
+    
 }
