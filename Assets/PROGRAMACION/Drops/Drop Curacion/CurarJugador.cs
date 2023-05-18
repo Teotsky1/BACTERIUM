757ce6b1bb49b9863d0e45338086ca7baa0000a7
Teotsky1 <mateo.arangog@upb.edu.co>
2023-05-18 00:41:21 -0500

    Spawns Curaciones

---
+++ Assets/PROGRAMACION/Drops/Drop Curacion/CurarJugador.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CurarJugador : MonoBehaviour
+{
+    [SerializeField] float Curacion;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            
+            collision.gameObject.GetComponent<SistemaDeVida>().Curar(Curacion);
+
+            Destroy(this.gameObject);
+        }
+    }
+}
